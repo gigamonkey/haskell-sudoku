8cc4f11ca0ea49711f714e183ae93e45fa2a8252
Peter Seibel <pseibel@twitter.com>
2014-07-09 14:36:56 -0700

    Code tidying.

--- sudoku.hs
+++ sudoku.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import Data.Array
 import Data.List
 import Data.Maybe
@@ -14,15 +15,13 @@ digits = "123456789"
 emptyBoard :: Board
 emptyBoard = listArray (0, 80) [ Nothing | _ <- [0..80] ]
 
-sq r c = r*9 + c
-
-rows  = [ [ sq r c | c <- [0..8] ] | r <- [0..8] ]
-cols  = [ [ sq r c | r <- [0..8] ] | c <- [0..8] ]
-boxes = [ [ sq r c | r <- [rs..rs+2], c <- [cs..cs+2] ] | rs <- [0,3,6], cs <- [0,3,6] ]
+rows  = [ [ r*9 + c | c <- [0..8] ] | r <- [0..8] ]
+cols  = [ [ r*9 + c | r <- [0..8] ] | c <- [0..8] ]
+boxes = [ [ r*9 + c | r <- [rs..rs+2], c <- [cs..cs+2] ] | rs <- [0,3,6], cs <- [0,3,6] ]
 
 squares   = [0..80]
 all_units = rows ++ cols ++ boxes
-units     = [ [ u | u <- all_units, s `elem` u ] | s <- squares ]
+units     = [ [ u | u <- all_units, elem s u ] | s <- squares ]
 peers     = [ delete s (foldl union [] (units !! s)) | s <- squares ]
 
 -- To and from textual representation
@@ -45,62 +44,34 @@ oneline = map unsquare . elems
 
 -- Solving code
 
-isEmpty (_, Nothing) = True
-isEmpty (_, (Just _)) = False
-
-emptySquare b =
-    case map fst $ filter isEmpty $ assocs b of
-      x:xs -> Just x
-      []   -> Nothing
-
-
 -- The main function
 solve :: Maybe Board -> Maybe Board
-solve Nothing = Nothing
+solve Nothing  = Nothing
 solve (Just b) = search (Just b) (emptySquare b) digits
 
--- The meat of it.
-search :: Maybe Board -> Maybe Square -> [Char] -> Maybe Board
-
--- We've already hit a dead end
-search Nothing _ _ = Nothing
-
--- We've run out of digits for the current square.
-search (Just _) (Just _)  [] = Nothing
-
--- No more empty squares, we're done!
-search (Just b) Nothing _ = Just b
-
--- Still work to do on the current square.
+search :: Maybe Board -> Maybe Square -> [Digit] -> Maybe Board
+search Nothing _ _              = Nothing -- dead end in assigment
+search (Just _) (Just _)  []    = Nothing -- ran out of digits to try
+search (Just b) Nothing _       = Just b  -- solved it!
 search (Just b) (Just s) (d:ds) =
     case assign b s d of
-      Nothing -> search (Just b) (Just s) ds
-      Just b' -> case solve (Just b') of
-                   Nothing -> search (Just b) (Just s) ds
-                   solution -> solution
+      Nothing -> tryNextDigit
+      next    -> nextStep next
+    where tryNextDigit = search (Just b) (Just s) ds
+          nextStep b'  = solve b' <|> tryNextDigit
 
--- Assign the given digit to the given square and return either Just
--- the new board or Nothing if the assignment leads to a contradiction.
-assign :: Board -> Square -> Digit -> Maybe Board
+-- Get an empty square to try filling.
+emptySquare b = listToMaybe $ map fst $ filter (isNothing . snd) $ assocs b
 
-setDigit :: Board -> (Square, Digit) -> Board
-setDigit b (s, d) = b // [(s, Just d)]
+-- Assign a given digit to the given square. Return Nothing if that
+-- leads to an immediate contradiction.
+assign b s d = if inPeers b s d then Nothing else Just (b // [(s, Just d)])
 
-assign b s d =
-    if inPeers b s d then Nothing else Just (setDigit b (s, d))
-
-inPeer :: Board -> Square -> Digit -> Square -> Bool
-inPeer b s d p =
-    case b ! p of
-      Nothing -> False
-      Just d' -> d == d'
-
-inPeers b s d = any (inPeer b s d) (peers !! s)
+inPeers b s d = any inPeer (peers !! s)
+    where inPeer p = maybe False (d==) (b ! p)
 
 main = do
   args <- getArgs
-  -- putStrLn $ show (units !! 0)
-  -- putStrLn $ show (peers !! 0)
   emit args
   where emit (a:as) = do
           puzzle <- readFile a
